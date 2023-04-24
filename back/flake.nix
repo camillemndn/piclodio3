78cb2259d5c27708b3e642c72d7c7dd69d7b3aaf
genesis <camillemondon@free.fr>
2023-04-24 12:06:34 +0200

    common: created flake and direnv for backend

---
+++ back/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = (with pkgs; [
+            python310
+            mpg123
+            killall
+          ]) ++ (with pkgs.python310.pkgs; [
+            pip
+            virtualenv
+          ]);
+        };
+      });
+}
+
