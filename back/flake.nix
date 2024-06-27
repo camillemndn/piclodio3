76f9131c74062d08f0c5e42b246510b1dedf80e7
Camille M. (genesis) <camillemondon@free.fr>
2024-06-27 14:17:22 +0200

    chore: update flake.lock

--- back/flake.nix
+++ back/flake.nix
@@ -3,43 +3,48 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShells.default =
           let
-            python-packages = p: with p; [
-              django
-              django-filter
-              djangorestframework
-              markdown
-              drf-yasg
-              apscheduler
-              sqlalchemy
-              psutil
-              django-cors-headers
-              gunicorn
-              pyyaml
-              packaging
-              (callPackage ./. { })
-            ];
+            python-packages =
+              p: with p; [
+                django
+                django-filter
+                djangorestframework
+                markdown
+                drf-yasg
+                apscheduler
+                sqlalchemy
+                psutil
+                django-cors-headers
+                gunicorn
+                pyyaml
+                packaging
+                (callPackage ./. { })
+              ];
           in
           pkgs.mkShell {
             nativeBuildInputs = [ pkgs.bashInteractive ];
             buildInputs = with pkgs; [
               mpv
               killall
-              spotify-tui
+              spotify-player
               (python3.withPackages python-packages)
             ];
           };
 
         packages.pyalsaaudio = pkgs.callPackage ./. { };
-      });
+      }
+    );
 }
-
-
-
